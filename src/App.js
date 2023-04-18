c62ea817850b617aa45d16a9dde254a8c1db5708
Taing Menghai <taing.menghai.2021@gmail.com>
2023-04-18 10:49:43 +0700

    add fractal tree

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import FractalTree from './FractalTree.js'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='App-header'>
+				<h1>Fractal Tree 🌳</h1>
+				{<FractalTree></FractalTree>}
+				<p>
+					A fractal tree is a type of geometric shape that has a self-similar
+					structure. This means that each branch of the tree is a smaller copy
+					of the whole tree, and this pattern repeats infinitely. A fractal tree
+					can be created by starting with a line segment and adding two smaller
+					line segments at an angle to each end. Then, the same process is
+					repeated for each new line segment, and so on. The result is a
+					beautiful and complex pattern that resembles a natural tree.
+				</p>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
